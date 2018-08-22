6e9cd3a224870e7fe230933f3811516702db716d
montekaka <josh.chen.jia@gmail.com>
2018-08-22 13:52:26 -0700

    add endpoint for create a new host and search for nearby wifi spots

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+const data = require('./../data/host_addresses.json');
 const app = express();
 
-
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
 //app.get('/', (req, res) => res.send('Hello world FFF'));
+
+app.get('/api/hosts', (req, res) => {
+	res.json(data);
+});
+
+
+app.post('/api/hosts', (req, res) => {
+	console.log('body...',req.body);
+	//throw new Error("BROKEN");
+	var successMsg = {"message": "Thanks for hosting your wifi with us"}
+	res.json(successMsg);
+});
+
+app.post('/api/guests/search', (req, res) => {
+	console.log('body...', req.body);
+	res.json(data.slice(0, 6));
+});
 
 
 
