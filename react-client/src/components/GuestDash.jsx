61db0a19ec4560efe596e0946e575ca259a9e3fd
Arianna Kellogg <ari@truss.works>
2018-08-27 21:56:12 -0700

    coded skeleton for index.jsx and index.html, added components folder

---
+++ react-client/src/components/GuestDash.jsx
@@ -0,0 +1,17 @@
+// GuestDash.jsx
+
+class GuestDashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // state is something in relation to props
+    }
+  }
+
+
+  render() {
+
+  }
+}
+
+export default GuestDashboard;
