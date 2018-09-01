4fc0ab59471033dc8c1fb23ceda0ee46f0e2e2e3
montekaka <josh.chen.jia@gmail.com>
2018-09-01 11:11:58 -0700

    add guest searching endpoint

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ app.use(express.static(__dirname + '/../client/dist'));
 // 	res.json(successMsg);
 // });
 
+// guest searching endpoint
 app.post('/api/guests/search', (req, res) => {
 	console.log('body...', req.body);
 	res.json(data.slice(0, 6));
@@ -56,6 +57,7 @@ app.route('/api/hosts/:hostId')
 		res.json({hostId: hostId});
 	})
 
+// guest
 
 
 
