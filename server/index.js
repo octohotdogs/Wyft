7702a4625c26de0e65187043e50731b9df00b8a9
montekaka <josh.chen.jia@gmail.com>
2018-09-01 15:18:35 -0700

    add sql package

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 const data = require('./../data/host_addresses.json');
-const db = require('../db/index.js');
+//const db = require('../db/index.js');
 const app = express();
 const hosts = require('./controllers/hosts.js');
 
