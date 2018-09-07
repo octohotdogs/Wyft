c223f537730e77b8cec25237c329beecb2521b05
Arianna Kellogg <ari@truss.works>
2018-09-06 20:51:18 -0700

    added components to render search results of hosts and render each host's info when their button is clicked

---
+++ client/src/components/HostResultsList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import HostInfoDetail from 'HostInfoDetail.jsx';
+
+class HostResultsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      availableHosts : []
+    }
+  }
+
+
+  render() {
+    // renders list of individual Host buttons
+      // when clicked
+        // each button renders individual host info
+  }
+
+
+}
+
+export default HostResultsList;
