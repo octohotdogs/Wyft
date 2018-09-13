dd97f3c67399a0a93a9f496fd9ba16d383fb3f32
Rosita Calderon <rpcalderon86@gmail.com>
2018-09-12 20:30:01 -0400

    Add styles

--- client/src/components/SessionsAdd.jsx
+++ client/src/components/SessionsAdd.jsx
@@ -10,75 +10,75 @@ import {
 } from 'semantic-ui-calendar-react';
 
 class SessionsAdd extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			DATE: '',
-			START_TIME: '',
-			END_TIME: '',
-			hostId: ''
-		}
+  constructor(props) {
+    super(props);
+    this.state = {
+      DATE: '',
+      START_TIME: '',
+      END_TIME: '',
+      hostId: ''
+    };
 
-		this.onSubmit = this.onSubmit.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-	}
+    this.onSubmit = this.onSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-
-  handleChange(event, {name, value}){
+  handleChange(event, { name, value }) {
     if (this.state.hasOwnProperty(name)) {
       this.setState({ [name]: value });
     }
   }
 
-  onSubmit(){
-  	this.setState({hostId: this.props.hostId});
-  	console.log(this.state);
-  	$.ajax({
-  		type: 'POST',
-  		url: `/api/hosts/${this.props.hostId}/sessions`,
-  		data: JSON.stringify({data: this.state}),
-  		contentType: 'application/json',
-	      success: function(data) {	      	
-	      	//TODO redirect the page to host profile
-	        console.log(data);
-	      },
+  onSubmit() {
+    this.setState({ hostId: this.props.hostId });
+    console.log(this.state);
+    $.ajax({
+      type: 'POST',
+      url: `/api/hosts/${this.props.hostId}/sessions`,
+      data: JSON.stringify({ data: this.state }),
+      contentType: 'application/json',
+      success: function(data) {
+        //TODO redirect the page to host profile
+        console.log(data);
+      },
       error: function(err) {
-      	console.log(err);
-      }  		
-  	})
+        console.log(err);
+      }
+    });
   }
 
-	render() {
-		return(
-			<div>
-				<Form>							
-					<Form.Group widths="equal">
-						<DateInput
-	          name="DATE"
-	          placeholder="Date"
-	          value={this.state.DATE}
-	          iconPosition="left"
-	          onChange={this.handleChange} />	
-					</Form.Group>	   
-					<Form.Group widths="equal">
-							<TimeInput 
-			        	name="START_TIME"
-			        	placeholder="Start time"
-			        	value={this.state.START_TIME}
-			        	onChange={this.handleChange}
-			        />
-			        <TimeInput 
-			        	name="END_TIME"
-			        	placeholder="End time"
-			        	value={this.state.END_TIME}
-			        	onChange={this.handleChange}
-			        />			        
-					</Form.Group>	  	        	      	      
-				</Form>
-				<button onClick={this.onSubmit} >Submit</button>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Form>
+          <Form.Group widths="equal">
+            <DateInput
+              name="DATE"
+              placeholder="Date"
+              value={this.state.DATE}
+              iconPosition="left"
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Group widths="equal">
+            <TimeInput
+              name="START_TIME"
+              placeholder="Start time"
+              value={this.state.START_TIME}
+              onChange={this.handleChange}
+            />
+            <TimeInput
+              name="END_TIME"
+              placeholder="End time"
+              value={this.state.END_TIME}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+        </Form>
+        <Button onClick={this.onSubmit}>Submit</Button>
+      </div>
+    );
+  }
 }
 
 export default SessionsAdd;
--- client/src/components/SessionsListItem.jsx
+++ client/src/components/SessionsListItem.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
+import { Icon, Label, Menu, Table } from 'semantic-ui-react';
 
 class SessionsListItem extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			data: ''			
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ''
+    };
+  }
 
-	// componentDidUpdate(prevProps){
-	// 	if(this.props.data !== prevProps.data) {
-	// 		this.setState({data: this.props.data});
-	// 	}
-	// }
+  // componentDidUpdate(prevProps){
+  // 	if(this.props.data !== prevProps.data) {
+  // 		this.setState({data: this.props.data});
+  // 	}
+  // }
 
-	render(){
-		return (
-			<div>
-				<div>Date: {this.props.data.DATE}</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      // <div>Date: {this.props.data.DATE}</div>
+      <Table celled>
+        {/* <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>First Name</Table.HeaderCell>
+            <Table.HeaderCell>Last Name</Table.HeaderCell>
+            <Table.HeaderCell>Date</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header> */}
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>{this.props.data.DATE}</Table.Cell>
+            <Table.Cell>{this.props.data.START_TIME}</Table.Cell>
+            <Table.Cell>{this.props.data.END_TIME}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    );
+  }
 }
-	
+
 export default SessionsListItem;
