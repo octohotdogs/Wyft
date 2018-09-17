14223bbf8e8973a9771c1fba54ca35860cc52a25
montekaka <josh.chen.jia@gmail.com>
2018-09-16 20:26:43 -0700

    add callback to get back clicked marker

--- client/src/components/GoogleMapKit.jsx
+++ client/src/components/GoogleMapKit.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import {getGoogleMaps, setMapMarker} from './../../../helpers/google_map/google-map-client.js'
 
+
 class GoogleMapKit extends React.Component {
 	constructor(props) {
 		super(props);
     this.state = {
-      guestLatLng: {lat: -25.363, lng: 131.044},
-      hostLatLngs: [],
-      map: ''
+      guestLatLng: {lat: 17.75, lng: 121.7333298},
+      hostLatLngs: [{
+            "id": 2,
+            "street_address": "0 Portage Center",
+            "lat": 38.9485485,
+            "lng": 22.8400018
+          }, {
+            "id": 3,
+            "street_address": "707 Springview Court",
+            "lat": 14.5016958,
+            "lng": 121.042743
+          }, {
+            "id": 4,
+            "street_address": "1 Algoma Crossing",
+            "lat": 59.2074685,
+            "lng": 9.6105211
+          }],
+      map: '',
+      clickedHost: ''
     }
     this.setupMap = this.setupMap.bind(this);
     this.setupHostMarkers = this.setupHostMarkers.bind(this);
 	}
 
   componentDidMount() {
-    this.setupMap();
+    // console.log(sectionsData)
+    var _this = this;
+    this.setupMap(function(){
+      _this.setupHostMarkers();
+    });
   }
 
-  setupMap() {
+  setupMap(cb) {
     var _this = this;
     getGoogleMaps('map', this.state.guestLatLng, function(map){
-      _this.setState({map: map});
+      _this.setState({map: map}, cb);
       setMapMarker(map, _this.state.guestLatLng, 'Guest');
     });
   }
@@ -29,8 +50,10 @@ class GoogleMapKit extends React.Component {
     var hostLatLngs = this.state.hostLatLngs;
     var map = this.state.map;    
     for(var i = 0; i < hostLatLngs.length; i++) {
-      var latLng = hostLatLngs[i];
-      setMapMarker(map, hostLatLngs, 'host');
+      var latLng = hostLatLngs[i];    
+      setMapMarker(map, latLng, 'host', (hostMarker) => {
+        console.log(hostMarker['street_address']);
+      });
     }
   }
 
