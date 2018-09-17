b6bb09aa553e12f9742613d138852fd9a444c510
montekaka <josh.chen.jia@gmail.com>
2018-09-16 20:26:21 -0700

    add addListener to marker to detech clicked marker

--- helpers/google_map/google-map-client.js
+++ helpers/google_map/google-map-client.js
@@ -6,11 +6,19 @@ var getGoogleMaps = (elementId, LatLng, cb) => {
 	cb(map);
 }
 
-var setMapMarker = (map, LatLng, title) => {
+var setMapMarker = (map, LatLng, title, cb) => {
   var marker = new google.maps.Marker({
   	position: LatLng,
   	map: map,
-  	title: title
+  	title: title,
+    wyft_session_id: LatLng['id'],
+    street_address: LatLng['street_address']
+  });
+
+  marker.addListener('click', () => {
+    if(cb !== undefined) {
+      cb(marker);
+    }    
   });
 }
 
